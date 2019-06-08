7f373f6e725bfafbca2a5f522bc81360ee0b0847
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-08 11:46:53 +0530

    Create PosNeg.java

---
+++ PosNeg.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PosNeg {
+    public static void main(String[]args){
+    	Scanner sc=new Scanner(System.in);
+    	int a=sc.nextInt();
+    	if(a>0){
+    		System.out.println("positive");
+    	}
+    	else{
+    		System.out.println("negative");
+    	}
+    }
+}
