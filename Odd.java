f721ac8ab73344c6c0a75b93153822a10d150fa0
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-08 12:00:16 +0530

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
-
 public class Odd {
 	public static void main(String args[]){
 		Scanner scan = new Scanner(System.in);
 		int a=scan.nextInt();
+		if(a>0){
 		if(a%2==0){
 			System.out.println("Even");
 		}
 		else{
 			System.out.println("Odd");
+		}}
+		else{
+			System.out.println("invalid");
 		}
 	}
 	
