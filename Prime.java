ab118f57bec0bc5b487337f6d0b148f8441c5206
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-10 11:45:57 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,21 +1,28 @@
+import java.util.Scanner;
+
 public class Prime {
 	 public static void main(String args[]){
 		 int p;
 	Scanner s = new Scanner(System.in); 
 	p=s.nextInt();
+	int ok=0;
 	for(int i=2; i<=p-1;i++){
-		
-	if(p%2==0){
-	int	ok=1;
-	System.out.println("not prime");
+		if(p%i==0){
+		ok=1;
 	break;
 	}
+	}
+		if(ok==1)
+		{
+			System.out.println("no");
+		}
 	else {
-		System.out.println(" prime");
-		break;
+		
+		System.out.println("yes");
 	}
 
 	}
-	}
 }               
 	 
+	
+	
