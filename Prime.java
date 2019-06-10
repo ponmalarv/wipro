99cf8db1109ef510e6d9fa6917ca319fea0d72cf
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-10 11:32:28 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,21 @@
+public class Prime {
+	 public static void main(String args[]){
+		 int p;
+	Scanner s = new Scanner(System.in); 
+	p=s.nextInt();
+	for(int i=2; i<=p-1;i++){
+		
+	if(p%2==0){
+	int	ok=1;
+	System.out.println("not prime");
+	break;
+	}
+	else {
+		System.out.println(" prime");
+		break;
+	}
+
+	}
+	}
+}               
+	 
