35c58e380c860c6e9bd82287f36814ee4434f663
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-10 10:25:04 +0530

    Create CountDigit.java

---
+++ CountDigit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CountDigit {
+
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int count=0;
+		while(n>0){
+			n=n/10;
+			count=count+1;
+		}
+		System.out.println(+count);
+	}
+}
