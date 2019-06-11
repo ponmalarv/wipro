c9f949719e3f51a19a225ad3578eba943cc84960
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-11 12:06:31 +0530

    Create FullStop.java

---
+++ FullStop.java
@@ -0,0 +1,10 @@
+mport java.util.Scanner;
+
+public class FullStop {
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+	String s1=s.next();
+	
+	System.out.println(s1+".");
+
+}}
