cffbb1ddace35aab62fd17e645ac720294de1d8b
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-11 17:32:21 +0530

    Update RevVowelsremove.java

--- RevVowelsremove.java
+++ RevVowelsremove.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class RevVowelsremove {
 	public static void main(String Args[]){
-
 		 Scanner s=new Scanner(System.in);
+		 int len=s.nextInt();
 		 String s1=s.next();
-		 s1=s1.replaceAll("[aeiouAEIOU]","");
+               s1=s1.replaceAll("[aeiouAEIOU]","");
 		 System.out.println(s1);
 		 StringBuffer sb=new StringBuffer(s1);
 		 System.out.println(sb.reverse());
 		s.close(); 
-		 
-}
+		} 
 }
