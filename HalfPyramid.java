355a256f756456f2f070294f4350e457bd7e67f0
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-06-14 17:23:39 +0530

    Create HalfPyramid.java

---
+++ HalfPyramid.java
@@ -0,0 +1,13 @@
+public class HalfPyramid {
+	public static void main(String args[]){
+		int r=8;
+		for(int i=1;i<=r;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(j+" ");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
