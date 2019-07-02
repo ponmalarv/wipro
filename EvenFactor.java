9aed199fa70d3e5bfd4d1b8af3f732ec9c24d5cb
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-07-02 17:59:50 +0530

    Create  EvenFactor.java

---
+++ EvenFactor.java
@@ -0,0 +1,15 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class EvenFactor {
+	public static void main(String args[]){
+		  Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		for(int i=1;i<=a;i++){
+			if(a%i==0){
+				System.out.println(i);
+			}
+		}
+}
+}
