eeea441979d3e683136b1132c4516dd098f59597
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-07-24 11:56:44 +0530

    Create MaxValue.java

---
+++ MaxValue.java
@@ -0,0 +1,21 @@
+package sums;
+
+import java.util.Scanner;
+
+public class MaxValue {
+public static void main(String args[]){
+	Scanner scan=new Scanner(System.in);
+	int n=scan.nextInt();
+	int val[]=new int[n];
+	for(int i=0;i<n;i++){
+		val[i]=scan.nextInt();
+	}
+	int max=val[0];
+	for(int i=1;i<val.length;i++){
+		if(val[i]>max){
+			max=val[i];
+		}
+	}
+	System.out.println(max);
+}
+}
