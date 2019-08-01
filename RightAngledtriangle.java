056aa907903945d151ef76a7471129648c4b4b0b
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-08-01 13:42:25 +0530

    Create RightAngledtriangle.java

---
+++ RightAngledtriangle.java
@@ -0,0 +1,16 @@
+package sums;
+import java.util.Scanner;
+public class RightAngledtriangle {
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+		int n=1;int p=0;
+		for(int i=0;i<num;i++){
+			for(int j=0;j<=p;j++){
+			System.out.print(n+" ");
+			
+		}System.out.println();
+		p+=2;
+		
+			}}
+}
