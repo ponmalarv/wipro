29afd06e54ffa2eb15d3a87f5d0a4ee44edcfc0d
ponmalarv <51502769+ponmalarv@users.noreply.github.com>
2019-08-24 14:24:30 +0530

    Create factors.java

---
+++ factors.java
@@ -0,0 +1,13 @@
+package GuviNew;
+import java.util.Scanner;
+public class factors {
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+				int n=scan.nextInt();
+				for(int i=1;i<=n;i++){
+					if(n%i==0){
+						System.out.println(i);
+					}
+				}
+}
+}
